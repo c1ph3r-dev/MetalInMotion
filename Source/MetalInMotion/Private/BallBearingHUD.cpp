e3b72c56097c81a06bb9bc694d4a6c818b8d529a
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2022-07-24 11:30:25 -0400

    Added Linux Build Support

--- Source/MetalInMotion/Private/BallBearingHUD.cpp
+++ Source/MetalInMotion/Private/BallBearingHUD.cpp
@@ -23,10 +23,16 @@ void ABallBearingHUD::DrawHUD()
 
 	if (BallBearing != nullptr)
 	{
-		AddBool(L"In contact", BallBearing->InContact);
-		AddFloat(L"Speed", BallBearing->GetVelocity().Size() / 100.0f);
-		AddFloat(L"Dash timer", BallBearing->DashTimer);
-		AddFloat(L"Input latitude", BallBearing->InputLatitude);
-		AddFloat(L"Input longitude", BallBearing->InputLongitude);
+	    const char* IC = "In contact"; size_t n = strlen(IC); const FString& Str_IC = FString(n, IC); const TCHAR* InContact = *Str_IC;
+		const char* Spd = "Speed"; n = strlen(Spd); const FString& Str_Spd = FString(n, Spd); const TCHAR* Speed = *Str_Spd;
+		const char* DT = "Dash timer"; n = strlen(DT); const FString& Str_DT = FString(n, DT); const TCHAR* DashTimer = *Str_DT;
+		const char* ILA = "Input latitude"; n = strlen(ILA); const FString& Str_ILA = FString(n, ILA); const TCHAR* InLatitude = *Str_ILA;
+		const char* ILO = "Input longitude"; n = strlen(ILO); const FString& Str_ILO = FString(n, ILO); const TCHAR* InLongitude = *Str_ILO;
+		
+		AddBool(InContact, BallBearing->InContact);
+		AddFloat(Speed, BallBearing->GetVelocity().Size() / 100.0f);
+		AddFloat(DashTimer, BallBearing->DashTimer);
+		AddFloat(InLatitude, BallBearing->InputLatitude);
+		AddFloat(InLongitude, BallBearing->InputLongitude);
 	}
 }
